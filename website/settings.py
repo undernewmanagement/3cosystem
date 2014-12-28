d2520c49dc362e3bfdc803d444a23b271be0dd34
undernewmanagement <sheraz@under-new-management.com>
2014-12-28 02:28:50 +0100

    cleaning up on how we did django signals

--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    #'tech_events.apps.TechEventsAppConfig',
     'tech_events',
     'geography',
     'location_field'
