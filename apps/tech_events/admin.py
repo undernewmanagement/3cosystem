e64da9b188db0eceb835ab3ce579f5866350676a
undernewmanagement <sheraz@under-new-management.com>
2015-01-03 14:24:19 +0100

    Added features: 1. added searching on tech events in admin 2. bulk action to mark selected events as inactive

--- apps/tech_events/admin.py
+++ apps/tech_events/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from apps.tech_events.models import MeetupGroup, TechEvent, ParseError
 
 
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+make_inactive.short_description = "Mark selected events inactive"
+
 class MeetupGroupAdmin(admin.ModelAdmin):
     exclude      = ('name','location',)
     list_display = ('name','url') 
     list_filter  = ('is_blacklisted',)
     search_fields = ['name','url']
 
+class TechEventAdmin(admin.ModelAdmin):
+    search_fields = ['url','name']
+    list_filter   = ('is_active',)
+    actions       = [make_inactive]
+
+
 
 admin.site.register(MeetupGroup, MeetupGroupAdmin)
-admin.site.register(TechEvent)
+admin.site.register(TechEvent, TechEventAdmin)
 admin.site.register(ParseError)
