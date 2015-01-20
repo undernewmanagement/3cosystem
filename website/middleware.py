c904cc98c80d7f29fb48a007134c97d99dfe6207
undernewmanagement <sheraz@under-new-management.com>
2015-01-20 03:42:25 +0100

    added naked domain redirection via middleware

---
+++ website/middleware.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponsePermanentRedirect
+
+class WWWRedirectMiddleware(object):
+    def process_request(self, request):
+        if request.META['HTTP_HOST'].startswith('3cosystem.'):
+            return HttpResponsePermanentRedirect('http://www.3cosystem.com')
--- website/settings.py
+++ website/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'middleware.WWWRedirectMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
