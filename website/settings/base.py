8683979ffa31de3951f2a0e2450895c19b3d454e
Mark Anderson <mark@m3b.net>
2016-05-23 10:59:08 +0200

    static assets were a mess

--- website/settings/base.py
+++ website/settings/base.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__)) + '/../'
 
+PROJECT_DIR=BASE_DIR
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'dist')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
