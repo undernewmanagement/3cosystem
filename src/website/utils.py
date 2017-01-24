b15f1bc981e772eab11d836918e3abef437c6c7b
Mark Anderson <mark@m3b.net>
2017-01-24 01:31:36 +0100

    Documentation, because holy shit I couldn't make heads or tails of this function. But now I do, and my knowledge is your knowledge. You're welcome :-)

--- src/website/utils.py
+++ src/website/utils.py
@@ -3,17 +3,51 @@ from collections import OrderedDict
 
 
 def reshape_events(events):
+    """
+    Received a list of TechEvent objects and returns an ordered dictionary
+    Args:
+        events: Array of TechEvent model objects
+
+    Returns:
+        OrderedDict: Ordered dictionary of events sorted by day ascending
+
+    Examples:
+        Output:
+            datetime.date(2017, 1, 24): {
+                'date': 'Jan 24',
+                'dow': 'Tuesday',
+                'events': [
+                    <TechEvent: TechEvent object>,
+                    <TechEvent: TechEvent object>,
+                    <TechEvent: TechEvent object>
+                ]
+            },
+            datetime.date(2017, 1, 25): {
+                'date': 'Jan 25',
+                'dow': 'Wednesday',
+                'events': [
+                    <TechEvent: TechEvent object>,
+                    <TechEvent: TechEvent object>
+                ]
+            },
+    """
     out = {}
+
+    # loop over all the events
     for e in events:
         k = e.begin_time.date()
         if k not in out:
             out[k] = {
+                # Format: Mmm dd (Mar 09)
                 'date': e.begin_time.strftime('%b %d'),
+
+                # Day of week: (Monday)
                 'dow': e.begin_time.strftime('%A'),
                 'events': []
             }
 
         out[k]['events'].append(e)
 
+    # sort the items
     return OrderedDict((datetime.strftime(k, '%d-%m-%Y'), v)
                        for k, v in sorted(out.items()))
