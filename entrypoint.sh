e5771e02f509efae047e20ff6ae187d881c2879d
Mark Anderson <mark@m3b.net>
2017-01-24 18:58:13 +0100

    forgot to update this with appropriate env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,12 @@
 set -e
 cmd="$@"
 
-# the official postgres image uses 'postgres' as default user if not set explictly.
-if [ -z "$POSTGRES_USER" ]; then
-    export POSTGRES_USER=postgres
-fi
-
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
-
 function postgres_ready(){
 python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASS", host="$DB_HOST")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
