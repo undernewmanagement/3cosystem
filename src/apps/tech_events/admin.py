b5f64c2a6fdf4e3a6b98679903c101f450c107c1
Mark Anderson <mark@m3b.net>
2017-01-28 21:46:26 +0100

    Bulk blacklisting/unblacklisting meetup groups in admin

--- src/apps/tech_events/admin.py
+++ src/apps/tech_events/admin.py
@@ -7,11 +7,23 @@ def make_inactive(modeladmin, request, queryset):
 make_inactive.short_description = "Mark selected events inactive"
 
 
+def make_blacklisted(modeladmin, request, queryset):
+    queryset.update(is_blacklisted=True)
+make_blacklisted.short_description = "Blacklist selected groups"
+
+
+def make_unblacklisted(modeladmin, request, queryset):
+    queryset.update(is_blacklisted=False)
+make_unblacklisted.short_description = "Un-Blacklist selected groups"
+
+
 class MeetupGroupAdmin(admin.ModelAdmin):
     exclude = ('name', 'location',)
     list_display = ('name', 'url')
     list_filter = ('is_blacklisted',)
     search_fields = ['name', 'url']
+    actions = [make_blacklisted, make_unblacklisted]
+
 
 class TechEventAdmin(admin.ModelAdmin):
     search_fields = ['url', 'name']
