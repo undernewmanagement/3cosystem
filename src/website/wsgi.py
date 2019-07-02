555cd9ae029195d71f0f4e33145016b4f1863662
Mark Anderson <mark@m3b.net>
2019-07-02 07:50:39 +0200

    whitenoise changed it behavior, so we no longer need to wrap the wsgi object here for production runs

--- src/website/wsgi.py
+++ src/website/wsgi.py
@@ -1,10 +1,7 @@
 import os
-from whitenoise.django import DjangoWhiteNoise
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
