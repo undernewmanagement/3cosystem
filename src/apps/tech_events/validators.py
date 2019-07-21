bd437fda76818a721583f01028ee7c0dcb084c21
Mark Anderson <mark@m3b.net>
2019-07-21 12:40:11 +0200

    update to https and fstrings

--- src/apps/tech_events/validators.py
+++ src/apps/tech_events/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 import requests
 
 def validate_meetup_url_exists(value):
-    r = requests.head('http://www.meetup.com/%s/events/ical/' % (value,))
+    r = requests.head(f"https://www.meetup.com/{value}/events/ical/")
     if r.status_code != 200:
-        raise ValidationError(u'%s is not a valid Meetup URL' % value)
+        raise ValidationError(f"{value} is not a valid Meetup URL")
