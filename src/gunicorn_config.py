4756aa296ca85624f3d8de82155877db9acdc5c9
Sam Texas <sam@simplecto.com>
2020-01-19 02:53:19 +0100

    bump django and change workers on gunicorn

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -1,7 +1,9 @@
 import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
+threads = 3
 accesslog = "-"
 errorlog = "-"
 forwarded_allow_ips = "*"
